629e3313f8da9f0472247bc5ef161186c1cc1cf5
vav1 <vav1@vgserv.ru>
2025-06-22 08:48:53 +0500

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"flag"
+	"os"
+)
+
+type RunConfig struct {
+	RunAddress string `env:"RUN_ADDRESS"`
+	DbURL      string `env:"DATABASE_URL"`
+	AccSysAddr string `env:"ACCRUAL_SYSTEM_ADDRESS"`
+}
+
+func Load() *RunConfig {
+	cfg := &RunConfig{}
+	flag.StringVar(&cfg.RunAddress, "a", "localhost:8080", "address and port to run server")
+	flag.StringVar(&cfg.DbURL, "d", "", "host=<host> user=<user> password=<password> dbname=<dbname> sslmode=<disable/enable>")
+	flag.StringVar(&cfg.AccSysAddr, "r", "", "accrual system address ")
+	flag.Parse()
+
+	if envRunAddress := os.Getenv("RUN_ADDRESS"); envRunAddress != "" {
+		cfg.RunAddress = envRunAddress
+	}
+	if envDbURL := os.Getenv("DATABASE_URL"); envDbURL != "" {
+		cfg.DbURL = envDbURL
+	}
+	if envAccSysAddr := os.Getenv("ACCRUAL_SYSTEM_ADDRESS"); envAccSysAddr != "" {
+		cfg.AccSysAddr = envAccSysAddr
+
+	}
+	return cfg
+}
